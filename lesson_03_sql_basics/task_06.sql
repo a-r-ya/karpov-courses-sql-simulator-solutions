38fa1c19d2300d621217da94d74004542ee9fd8e
Alina Yalysheva <53533684+a-r-ya@users.noreply.github.com>
2025-07-09 13:52:33 +0300

    Update and rename task_06.sql to lesson_03_task_08.sql

--- lesson_03_sql_basics/task_06.sql
+++
@@ -1,9 +0,0 @@
-/*
-Измените тип колонки price из таблицы products на VARCHAR.
-Выведите только эту колонку.
-Поле в результирующей таблице: price
-*/
-SELECT
-  CAST(price AS VARCHAR)
-FROM
-  products;
