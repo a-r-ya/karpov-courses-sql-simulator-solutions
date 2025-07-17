6b305bda58243e2be1c4cc2f2a9b12cfe7affc7b
Alina Yalysheva <53533684+a-r-ya@users.noreply.github.com>
2025-07-17 13:47:13 +0300

    Update lesson_04_task_09.sql

--- lesson_04_filtering/lesson_04_task_09.sql
+++ lesson_04_filtering/lesson_04_task_09.sql
@@ -13,7 +13,6 @@ FROM
   user_actions
 WHERE
   user_id IN (170, 200, 230)
-  AND time BETWEEN '2022-08-25'
-  AND '2022-09-05'
+  AND time BETWEEN '2022-08-25' AND '2022-09-05'
 ORDER BY
   order_id DESC;
