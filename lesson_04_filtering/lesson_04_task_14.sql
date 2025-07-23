205f8e115d4e0e9717cf2516ef20ce92e65944c1
Alina Yalysheva <53533684+a-r-ya@users.noreply.github.com>
2025-07-23 18:00:31 +0300

    Update and rename task_10.sql to lesson_04_task_14.sql

---
+++ lesson_04_filtering/lesson_04_task_14.sql
@@ -0,0 +1,14 @@
+/*
+Из таблицы couriers отберите id всех курьеров, 
+родившихся в период с 1990 по 1995 год включительно.
+Результат отсортируйте по возрастанию id курьера.
+Поле в результирующей таблице: courier_id
+*/
+SELECT
+  courier_id
+FROM
+  couriers
+WHERE
+  DATE_PART('year', birth_date) BETWEEN 1990 AND 1995
+ORDER BY
+  courier_id;
