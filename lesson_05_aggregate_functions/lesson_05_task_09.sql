8facfc480f5f3a61408d7f3dd434bccec6b97b81
Alina Yalysheva <53533684+a-r-ya@users.noreply.github.com>
2025-07-29 17:46:41 +0300

    Update and rename task_06.sql to lesson_05_task_09.sql

--- lesson_05_aggregate_functions/lesson_05_task_09.sql
+++ lesson_05_aggregate_functions/lesson_05_task_09.sql
@@ -2,12 +2,12 @@
 Посчитайте количество заказов в таблице orders с девятью и более товарами. 
 Для этого воспользуйтесь функцией array_length, 
 отфильтруйте данные по количеству товаров в заказе и проведите агрегацию. 
-Полученный столбец назовите orders_count.
-Поле в результирующей таблице: orders_count
+Полученный столбец назовите orders.
+Поле в результирующей таблице: orders
 */
 SELECT
-  COUNT(DISTINCT order_id) AS orders_count
+  COUNT(DISTINCT order_id) AS orders
 FROM
   orders
 WHERE
-  array_length(product_ids, 1) >= 9;
+  ARRAY_LENGTH(product_ids, 1) >= 9;
